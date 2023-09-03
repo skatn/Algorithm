f2518d14cec0741185a710bf0e5ce1bd68421a8e
skatn <87296704+skatn@users.noreply.github.com>
2023-09-03 19:37:56 +0900

    백준 2018

---
+++ java/src/main/java/beakjoon/_2018/Main.java
@@ -0,0 +1,57 @@
+package beakjoon._2018;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        solution2();
+    }
+
+    private static void solution1() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    count++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    //투 포인터
+    private static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        int start = 1;
+        int end = 1;
+
+        int sum = 1;
+
+        while (end < n) {
+            if (sum < n) {
+                sum += ++end;
+            } else if (sum > n) {
+                sum -= start++;
+            } else {
+                count++;
+                sum += ++end;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+
+}
