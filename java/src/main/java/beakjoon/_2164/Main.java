cf7aff6e51979ba9f15461713baffd32708897ba
skatn <87296704+skatn@users.noreply.github.com>
2023-12-07 00:41:51 +0900

    백준 2164

---
+++ java/src/main/java/beakjoon/_2164/Main.java
@@ -0,0 +1,27 @@
+package beakjoon._2164;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 백준 2164 - 카드2 (https://www.acmicpc.net/problem/2164)
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
