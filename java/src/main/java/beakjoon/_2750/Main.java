429607513ffe39bec2a05a462f006a9c5440e48d
skatn <87296704+skatn@users.noreply.github.com>
2023-12-09 20:21:40 +0900

    [알고리즘] 백준 2750

---
+++ java/src/main/java/beakjoon/_2750/Main.java
@@ -0,0 +1,35 @@
+package beakjoon._2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 백준 2750 - 수 정렬하기 (https://www.acmicpc.net/problem/2750)
+ * 알고리즘 : 버블정렬
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
