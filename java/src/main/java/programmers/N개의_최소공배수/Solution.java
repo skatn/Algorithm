e72dead3fccc9b5aecc7bbc4faceb706cd69ab2a
skatn <87296704+skatn@users.noreply.github.com>
2023-12-16 12:29:04 +0900

    [알고리즘] 프로그래머스 - N개의 최소공배수

---
+++ java/src/main/java/programmers/N개의_최소공배수/Solution.java
@@ -0,0 +1,24 @@
+package programmers.N개의_최소공배수;
+
+/**
+ * 프로그래머스 - N개의 최소공배수 (https://school.programmers.co.kr/learn/courses/30/lessons/12953)
+ * <p>
+ *     알고리즘 : 유클리드 호제법 <br>
+ *     최소공배수 = 두 수의 곱 / 최대공약수
+ * </p>
+ */
+public class Solution {
+
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            answer = answer * arr[i] / gcd(answer, arr[i]);
+        }
+        return answer;
+    }
+
+    public int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
---
+++ java/src/test/java/programmers/N개의_최소공배수/SolutionTest.java
@@ -0,0 +1,77 @@
+package programmers.N개의_최소공배수;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    void case1() {
+        // given
+        int[] arr = {2, 6, 8, 14};
+
+        // when
+        int answer = solution.solution(arr);
+
+        // then
+        assertEquals(168, answer);
+    }
+
+    @Test
+    void case2() {
+        // given
+        int[] arr = {1, 2, 3};
+
+        // when
+        int answer = solution.solution(arr);
+
+        // then
+        assertEquals(6, answer);
+    }
+
+    @Test
+    void case3() {
+        // given
+        int[] arr = {3, 5, 7, 9};
+
+        // when
+        int answer = solution.solution(arr);
+
+        // then
+        assertEquals(315, answer);
+    }
+
+    @Test
+    void case4() {
+        // given
+        int[] arr = {24, 40, 50};
+
+        // when
+        int answer = solution.solution(arr);
+
+        // then
+        assertEquals(answer, 600);
+    }
+
+    @Test
+    void case5() {
+        // given
+        int[] arr = {12, 44, 10, 8, 18, 20, 26};
+
+        // when
+        int answer = solution.solution(arr);
+
+        // then
+        assertEquals(51480, answer);
+    }
+
+}
