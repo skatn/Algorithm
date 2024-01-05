29d4581448debc069150c467407c80f5c7a23211
skatn <87296704+skatn@users.noreply.github.com>
2024-01-06 08:47:23 +0900

    [알고리즘] 프로그래머스 - 붕대 감기

---
+++ java/src/main/java/programmers/PCCP_기출문제_1번_붕대감기/Solution.java
@@ -0,0 +1,29 @@
+package programmers.PCCP_기출문제_1번_붕대감기;
+
+/**
+ * 프로그래머스 - 붕대 감기 (https://school.programmers.co.kr/learn/courses/30/lessons/250137)
+ */
+public class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int time = 0;           // 마지막 으로 공격 받은 시간
+        int maxHealth = health; // 최대 체력
+
+        for (int[] attack : attacks) {
+            int temp = attack[0] - time - 1;    // 붕대 감은 시간 계산
+            time = attack[0];                   // 마지막 으로 공격 받은 시간 갱신
+
+            // 체력 회복
+            // 붕대 감은 시간 * 초당 회복 시간 + (붕대 감은 시간 / 스킬 시전 시간) * 추가 회복량
+            // => 붕대 감은 시간 * 초당 회복 시간 + 스킬 완료 횟수 * 추가 회복량
+            health += temp * bandage[1] + (temp/bandage[0]) * bandage[2];
+            if (health > maxHealth) {
+                health = maxHealth;
+            }
+
+            health -= attack[1];
+            if(health <= 0) return -1;
+        }
+
+        return health;
+    }
+}
---
+++ java/src/test/java/programmers/PCCP_기출문제_1번_붕대감기/SolutionTest.java
@@ -0,0 +1,72 @@
+package programmers.PCCP_기출문제_1번_붕대감기;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void case1() {
+        // given
+        int[] bandage = {5, 1, 5};
+        int health = 30;
+        int[][] attacks = {{2, 10}, {9, 15}, {10, 5}, {11, 5}};
+
+        // when
+        int answer = solution.solution(bandage, health, attacks);
+
+        // then
+        assertEquals(5, answer);
+    }
+
+    @Test
+    void case2() {
+        // given
+        int[] bandage = {3, 2, 7};
+        int health = 20;
+        int[][] attacks = {{1, 15}, {5, 16}, {8, 6}};
+
+        // when
+        int answer = solution.solution(bandage, health, attacks);
+
+        // then
+        assertEquals(-1, answer);
+    }
+
+    @Test
+    void case3() {
+        // given
+        int[] bandage = {4, 2, 7};
+        int health = 20;
+        int[][] attacks = {{1, 15}, {5, 16}, {8, 6}};
+
+        // when
+        int answer = solution.solution(bandage, health, attacks);
+
+        // then
+        assertEquals(-1, answer);
+    }
+
+    @Test
+    void case4() {
+        // given
+        int[] bandage = {1, 1, 1};
+        int health = 5;
+        int[][] attacks = {{1, 2}, {3, 2}};
+
+        // when
+        int answer = solution.solution(bandage, health, attacks);
+
+        // then
+        assertEquals(3, answer);
+    }
+}
