1929fbd57e44bd4a613889c75ab6ffb9c1ad5c63
skatn <87296704+skatn@users.noreply.github.com>
2024-01-28 08:21:58 +0900

    [알고리즘] 퀵정렬

--- java/src/test/java/알고리즘/퀵정렬/QuickSortTest.java
+++ java/src/test/java/알고리즘/퀵정렬/QuickSortTest.java
@@ -12,7 +12,6 @@ class QuickSortTest {
     void case1() {
         int[] arr = {10, 5, 2, 3, 1, 4, 2, 3, 5, 1, 7};
         QuickSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
         assertArrayEquals(new int[]{1, 1, 2, 2, 3, 3, 4, 5, 5, 7, 10}, arr);
     }
 
